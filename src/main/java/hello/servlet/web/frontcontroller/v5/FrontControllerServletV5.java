e07a7a01a58ebb0e783c4a2f5187c03c4e9b8a9b
Soo77 <shimsh3@naver.com>
2023-12-25 23:03:46 +0900

    스프링 MVC 시작하기

--- src/main/java/hello/servlet/web/frontcontroller/v5/FrontControllerServletV5.java
+++ src/main/java/hello/servlet/web/frontcontroller/v5/FrontControllerServletV5.java
@@ -5,7 +5,11 @@ import hello.servlet.web.frontcontroller.MyView;
 import hello.servlet.web.frontcontroller.v3.controller.MemberFormControllerV3;
 import hello.servlet.web.frontcontroller.v3.controller.MemberListControllerV3;
 import hello.servlet.web.frontcontroller.v3.controller.MemberSaveControllerV3;
+import hello.servlet.web.frontcontroller.v4.controller.MemberFormControllerV4;
+import hello.servlet.web.frontcontroller.v4.controller.MemberListControllerV4;
+import hello.servlet.web.frontcontroller.v4.controller.MemberSaveControllerV4;
 import hello.servlet.web.frontcontroller.v5.adapter.ControllerV3HandlerAdapter;
+import hello.servlet.web.frontcontroller.v5.adapter.ControllerV4HandlerAdapter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -18,6 +22,37 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/*
+* 정리
+* 지금까지 v1~v4=5로 점진적으로 프레임워크를 발전시켜 왔다.
+* 지금까지 한 작업을 정리해보자.
+*
+* v1: 프론트 컨트롤러를 도입
+* 기존 구조를 최대한 유지하면서 프론트 컨트롤러를 도입
+*
+* v2: View 분류
+* 단순 반복되는 뷰 로직 분리
+*
+* v3: Model 추가
+* 서블릿 종속성 제거
+* 뷰 이름 중복 제거
+*
+* v4: 단순하고 실용적인 컨트롤러
+* v3와 거의 비슷,
+* 구현 입장에서 ModelView 를 직접 생성해서 반환하지 않도록 편리한 인터페이스 제공
+*
+* v5: 유연한 컨트롤러
+* 어댑터 도입
+* 어댑터를 추가해서 프레임 웤를 유연하고 확장성 있게 설계
+*
+* 여기에 에노테이션을 사용해서 컨트롤러를 더 편리하게 발전시킬 수도 있다. 만약 애노테이션을 사용해서 컨트롤러를 편리하게
+* 사용할 수 이 게 하려면 어떻게 해야할까? 바로 애노테이션을 지원하는 어댑터를 추가하면 된다!
+* 다형성과 어댑터 덕분에 기존 구조를 유지하면서, 프레임 워크의 기능을 확장할 수 있다.
+*
+* 스프링 MVC
+* 여기서 더 발전시키면 좋겠지만 , 스프링 MVC의 핵심 구조를 파악하는데 필요한 부분은 모두 만들어보았다.
+*
+* */
 @WebServlet(name = "frontControllerServletV5", urlPatterns = "/front-controller/v5/*")
 public class FrontControllerServletV5 extends HttpServlet {
 
@@ -34,10 +69,16 @@ public class FrontControllerServletV5 extends HttpServlet {
         handlerMappingMap.put("/front-controller/v5/v3/members/new-form", new MemberFormControllerV3());
         handlerMappingMap.put("/front-controller/v5/v3/members/save", new MemberSaveControllerV3());
         handlerMappingMap.put("/front-controller/v5/v3/members", new MemberListControllerV3());
+
+        handlerMappingMap.put("/front-controller/v5/v4/members/new-form", new MemberFormControllerV4());
+        handlerMappingMap.put("/front-controller/v5/v4/members/save", new MemberSaveControllerV4());
+        handlerMappingMap.put("/front-controller/v5/v4/members", new MemberListControllerV4());
+
     }
 
     private void initHandlerApaters() {
         handlerAdapters.add(new ControllerV3HandlerAdapter());
+        handlerAdapters.add(new ControllerV4HandlerAdapter());
     }
 
     @Override
